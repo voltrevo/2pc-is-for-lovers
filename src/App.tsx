ab79a08121a3f070c65f8d6105beb1adc15e2f7e
Andrew Morris <voltrevo@gmail.com>
2024-05-01 11:23:27 +1000

    Design first page

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,60 @@ import { useState } from 'react';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [, setCount] = useState(0);
 
   return (
     <>
-      <h1>2PC is for Lovers</h1>
-      <div className='card'>
+      <div className='title'>2PC IS FOR</div>
+      <div className='title lovers'>LOVERS</div>
+      <div className='title heart'>❤️</div>
+      <p>
+        Did you ever fall in love with your best friend? Hope they feel the
+        same, but afraid to lose the friendship?
+      </p>
+      <p>
+        This app uses advanced cryptography to solve the problem! 🤓
+      </p>
+      <h2>How it Works</h2>
+      <ol>
+        <li>Host a session.</li>
+        <li>Get your friend to join.</li>
+        <li>Choose love or friendship.</li>
+        <li>
+          If you both chose love, you'll both find out. Otherwise, you'll both
+          see friendship.
+        </li>
+      </ol>
+      <h2>They <i>Really</i> Won't Know?</h2>
+      <p>
+        Yes. Really.
+      </p>
+      <p>
+        This is an&nbsp;
+        <a href='https://github.com/voltrevo/2pc-is-for-lovers'>open source</a>
+        &nbsp;app.
+      </p>
+      <p>
+        If you choose love but the result is
+        friendship, <i>only you</i> will know. Even if your friend knows
+        advanced cryptography.
+      </p>
+      <p>
+        This is the <a href='https://www.youtube.com/watch?v=PzcDqegGoKI'>
+          magic
+        </a> of 2PC.
+      </p>
+      <p>
+        All communication is end-to-end encrypted. The server/internet will not
+        know either.
+      </p>
+      <div className='main buttons'>
         <button onClick={() => setCount(count => count + 1)}>
-          count is {count}
+          Host
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount(count => count + 1)}>
+          Join
+        </button>
       </div>
     </>
   );

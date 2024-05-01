cde69a3485296922033fb01f3848116521086c5e
Andrew Morris <voltrevo@gmail.com>
2024-05-02 09:14:48 +1000

    Make pasting code work

--- src/Join.tsx
+++ src/Join.tsx
@@ -1,5 +1,7 @@
+import * as bs58 from 'bs58';
 import { Scanner } from '@yudiel/react-qr-scanner';
 import Ctx from './Ctx';
+import { ChangeEvent } from 'react';
 
 export default function Join() {
   const ctx = Ctx.use();
@@ -10,7 +12,9 @@ export default function Join() {
       <p>Scan your friend's QR code:</p>
       <Scanner
         onResult={(text, _result) => {
-          ctx.join(text);
+          if (isKey(text)) {
+            ctx.join(text);
+          }
         }}
         onError={error => console.log(error?.message)}
         components={{
@@ -22,8 +26,21 @@ export default function Join() {
         <input
           type='text'
           style={{ width: '100%' }}
+          onInput={(ev: ChangeEvent<HTMLInputElement>) => {
+            if (isKey(ev.target.value)) {
+              ctx.join(ev.target.value);
+            }
+          }}
         />
       </p>
     </div>
   );
+}
+
+function isKey(text: string) {
+  try {
+    return bs58.decode(text).length === 32;
+  } catch {
+    return false;
+  }
 }

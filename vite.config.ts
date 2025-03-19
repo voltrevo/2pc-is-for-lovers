aa7e220b30b7ae1144aee9eb886a6ad9bda68f2f
Andrew Morris <voltrevo@gmail.com>
2025-03-19 13:43:33 +1100

    Add rtc config and base config

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2pc-is-for-lovers/',
+  base: process.env.VITE_2PC_IS_FOR_LOVERS_BASE ?? '/2pc-is-for-lovers/',
   plugins: [react()],
 });
